78600ddd5aa80aef38d6a830c4a01d049879df7d
Hui Peng <benquike@gmail.com>
2021-06-10 15:00:22 -0700

    Update examples/rubber-ducky.py

--- examples/rubber-ducky.py
+++ examples/rubber-ducky.py
@@ -11,27 +11,26 @@ from facedancer import main
 from facedancer.devices.keyboard     import USBKeyboardDevice
 from facedancer.classes.hid.keyboard import KeyboardModifiers
 
-device = USBKeyboardDevice()
+if __name__ == '__main__':
 
-async def type_letters():
-    logging.info("Beginning message typing demo...")
+    async def type_letters():
+        logging.info("Beginning message typing demo...")
 
-    # Type ls.
-    await asyncio.sleep(5)
-    await device.type_letters('l', 's', '\n')
+        # Type ls.
+        await asyncio.sleep(5)
+        await device.type_letters('l', 's', '\n')
 
-    # Echo hi.
-    await asyncio.sleep(2)
-    await device.type_string("echo hi, user\n")
+        # Echo hi.
+        await asyncio.sleep(2)
+        await device.type_string("echo hi, $USER\n")
 
-    # Finally, try to pop calc, just for fun.
-    logging.info("Bonus: trying to pop calc.")
-    await device.type_string('r', modifiers=KeyboardModifiers.MOD_LEFT_META)
-    await asyncio.sleep(0.5)
-    await device.type_string('calc\n')
+        # Finally, try to pop calc, just for fun.
+        logging.info("Bonus: trying to pop calc.")
+        await device.type_string('r', modifiers=KeyboardModifiers.MOD_LEFT_META)
+        await asyncio.sleep(0.5)
+        await device.type_string('calc\n')
 
+        logging.info("Typing complete. Idly handling USB requests.")
 
-    logging.info("Typing complete. Idly handling USB requests.")
-
-
-main(device, type_letters())
+    device = USBKeyboardDevice()
+    main(device, type_letters())
