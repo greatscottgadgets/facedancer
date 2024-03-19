a0d2dc306ee65dfee72187b1af7b3bfdf9ba600f
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-03-19 17:00:09 +0200

    Fix numeric value of LOGLEVEL_TRACE

--- facedancer/logging.py
+++ facedancer/logging.py
@@ -3,7 +3,7 @@ import logging
 import sys
 
 
-LOGLEVEL_TRACE = logging.DEBUG + 2
+LOGLEVEL_TRACE = 5
 
 LOG_FORMAT_COLOR = "\u001b[37;1m%(levelname)-8s| \u001b[0m\u001b[1m%(module)-15s|\u001b[0m %(message)s"
 LOG_FORMAT_PLAIN = "%(levelname)-8s| %(module)-15s| %(message)s"
