a89239c2c7b0a8ca6c8776aef4195d1b981255e4
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-06-14 14:58:59 +0200

    examples: remove platform-specific examples from rubbery-ducky

--- examples/rubber-ducky.py
+++ examples/rubber-ducky.py
@@ -16,20 +16,8 @@ device = USBKeyboardDevice()
 async def type_letters():
     logging.info("Beginning message typing demo...")
 
-    # Type ls.
-    await asyncio.sleep(5)
-    await device.type_letters('l', 's', '\n')
-
-    # Echo hi.
     await asyncio.sleep(2)
-    await device.type_string("echo hi, user\n")
-
-    # Finally, try to pop calc, just for fun.
-    logging.info("Bonus: trying to pop calc.")
-    await device.type_string('r', modifiers=KeyboardModifiers.MOD_LEFT_META)
-    await asyncio.sleep(0.5)
-    await device.type_string('calc\n')
-
+    await device.type_string("echo Hello, Facedancer!\n")
 
     logging.info("Typing complete. Idly handling USB requests.")
 
